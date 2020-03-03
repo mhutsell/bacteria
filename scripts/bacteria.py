7ff65c0707d93908283a2bfec34f07ee4dcfd150
Mack Hutsell <mack.hutsell@gmail.com>
2020-03-03 16:02:24 -0500

    Read stuff better and fix lin regression

--- scripts/bacteria.py
+++ scripts/bacteria.py
@@ -6,127 +6,43 @@ Created on Mon Mar  2 14:39:43 2020
 @authors: Mack, Camilla, Matthew
 """
 
+
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import scipy.stats as sp
 import scipy.integrate as odeint
 
-y_val_string = """3.00E+01
-4.00E+01
-2.00E+01
-3.00E+01
-1.20E+02
-1.25E+02
-2.35E+02
-2.45E+02
-3.65E+02
-3.30E+02
-7.85E+02
-8.20E+02
-8.84E+03
-3.88E+03
-3.70E+03
-1.55E+05
-1.77E+04
-1.90E+04
-3.00E+04
-8.70E+04
-7.04E+04
-3.10E+05
-5.00E+05
-5.00E+05
-5.50E+07
-4.20E+07
-5.50E+07
-4.26E+07
-1.13E+08
-7.58E+07
-1.26E+08
-1.53E+08
-7.95E+07
-9.95E+07
-1.19E+08
-5.32E+07
-1.98E+08
-8.02E+07
-1.29E+08
-1.17E+08
-1.14E+08
-9.70E+07
-9.50E+07
-7.90E+07
-9.44E+07
-8.95E+07
-2.40E+02
-2.90E+02
-4.40E+02
-5.90E+02
-1.29E+03
-6.80E+02"""
+data = pd.read_excel("../data/Bacteria_data.xlsx")
 
-x_val_string = """4
-4
-5
-5
-6
-6
-7
-7
-8
-8
-9
-9
-11
-11
-11
-13.03
-13.03
-13.03
-14.56
-14.56
-14.56
-18.22
-18.22
-18.22
-24
-24
-24
-22
-22
-24
-24
-25.5
-25.5
-35
-35
-38
-38
-41
-41
-60
-64
-64
-78
-78
-100
-100
-6
-6
-7
-7
-8
-8
-"""
+data.plot(kind = "scatter", x = "X", y="Y")
+plt.figure()
 
-
-y_vals = [float(y) for y in y_val_string.split()]
-x_vals = [float(x) for x in x_val_string.split()]
-value_dict = {}
-for i, x in enumerate(x_vals):
-    value_dict[x] = y_vals[i] 
+y_vals = data["Y"]
+x_vals = data["X"]
     
 x_vals_sorted = sorted(x_vals)
-y_vals_sorted = [value_dict[x] for x in x_vals_sorted]
-y_vals_log = [np.log(y) for y in y_vals_sorted]
-plt.plot(x_vals_sorted, y_vals_sorted, 'ro')
+
+y_vals_log = [np.log(y) for y in y_vals]
+
+
+
+
+################### Section 1: Lin Fit for finding gmax and mu
+
+x_vals_1 = x_vals[0:32]
+y_vals_1 = y_vals_log[0:32]
+np_y_1 = np.array(y_vals_1)
+np_x_1 = np.array(x_vals_1)
+np_x_1 = np_x_1.reshape((np_x_1.size, 1))
+np_y_1 = np_y_1.reshape((np_y_1.size, 1))
+
+a = float(np.linalg.lstsq(np_x_1, np_y_1, rcond=None)[0])
+m, b = sp.linregress(x_vals_1, y_vals_1)[0:2]
+print(m, b)
+
+plt.plot(x_vals_1, y_vals_1, 'ro')
 plt.figure()
-plt.plot(x_vals_sorted, y_vals_log, 'ro')
+y_vals_guessed_at = [a*x + b for x in x_vals_1]
+plt.plot(x_vals_1, y_vals_guessed_at, 'ro')
+print(a) # This equals gmax - mu, but mu is super small so probably just gmax
